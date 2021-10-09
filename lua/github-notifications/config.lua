d90f8a7a9ded1d4504c964d31cde7b7e282ac3cc
rlch <rjmbowie@gmail.com>
2021-10-10 01:27:18 +1100

    feat: statusline notification count

---
+++ lua/github-notifications/config.lua
@@ -0,0 +1,28 @@
+local secrets = require("github-notifications.secrets")
+
+local M = {}
+
+local defaults = {
+	debounce_duration = 60,
+	username = secrets.username,
+	token = secrets.token,
+}
+local mt = { __index = defaults }
+local config = setmetatable({}, mt)
+
+M.get = function(key)
+	if key then
+		return config[key]
+	end
+	return config
+end
+
+M.set = function(user_config)
+	if not user_config or type(user_config) ~= "table" then
+    return config
+	end
+	config = setmetatable(user_config, mt)
+  return config
+end
+
+return M
--- lua/github-notifications/init.lua
+++ lua/github-notifications/init.lua
@@ -1,24 +1,99 @@
-local M = {}
+local curl = require("plenary.curl")
+local config = require("github-notifications.config")
+local header = require("github-notifications.utils.header")
 
-local curl = require'plenary.curl'
+local M = {
+	notification_count = 0,
+	statusline_notification_count = function() end,
+}
 
-M.setup = function()
-  local res = curl.get('https://api.github.com/notifications', {
-    accept = 'application/json',
-    auth = USER .. ':' .. TOKEN,
-  })
+local state = {
+	last_refresh = nil,
+	poll = nil,
+}
 
-  local count = 0
-  local json = vim.fn.json_decode(res.body)
+-- Setup user configuration
+M.setup = function(options)
+	options = config.set(options)
+	print(options.debounce_duration)
+	state.debounce_duration = options.debounce_duration
+	M.refresh()
+end
 
-  for _, v in pairs(json) do
-    if v ~= nil then
-      print(v.subject.title)
-      count = count + 1
-    end
-  end
+-- Returns whether enough time has passed s.t. the user is able to make a new request.
+local debounce = function()
+	if not state.last_refresh then
+		return true
+	end
 
-  print(count)
+	print("dif: " .. tostring(state.last_refresh + state.debounce_duration - os.time()))
+	print("last modified:" .. tostring(state.last_refresh and header.last_modified(state.last_refresh) or nil))
+	local debounce_ok_at = state.last_refresh + state.debounce_duration
+	return debounce_ok_at < os.time()
+end
+
+-- Makes a new get request to the notifications API, refreshing the state variables.
+M.refresh = function()
+	if not debounce() then
+		print("Not enough time has passed!")
+		return
+	end
+
+	local res = curl.get("https://api.github.com/notifications", {
+		accept = "application/json",
+		auth = config.get("username") .. ":" .. config.get("token"),
+		headers = {
+			if_modified_since = state.last_refresh and header.last_modified(state.last_refresh) or nil,
+		},
+	})
+
+	local status = res.status
+	print(status)
+
+	if status == 200 then
+		local json = vim.fn.json_decode(res.body)
+
+		local count = 0
+		for _, v in pairs(json) do
+			if v ~= nil then
+				print(v.subject.title)
+				count = count + 1
+			end
+		end
+		M.notification_count = count
+		M.statusline_notification_count = function()
+			return " " .. tostring(count)
+		end
+
+		print("status: " .. res.status)
+		-- print("headers: " .. res.headers)
+		print("count: " .. count)
+		print(M.statusline_notification_count)
+
+		-- Reduce the debounce duration if necessary
+		for _, v in pairs(res.headers) do
+			local interval_match = v:match("x%-poll%-interval: (%d+)")
+			if interval_match then
+				local interval = tonumber(interval_match)
+				local dd = state.debounce_duration
+				print("interval: " .. tostring(interval) .. " dd: " .. tostring(dd))
+
+				-- In the latter case, we should revert to the user's config
+				state.debounce_duration = interval > dd and interval or config.get("debounce_duration")
+			end
+		end
+
+		state.last_refresh = os.time()
+	elseif status == 304 then
+		print("Not modified!")
+		return
+	elseif status == 401 then
+		print("Unauthorized")
+	elseif status == 403 then
+		print("Forbidden")
+	elseif status == 422 then
+		print("Validation failure")
+	end
 end
 
 return M
